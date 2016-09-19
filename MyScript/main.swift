9db8c8002e10c34f7fdbcbbce65ffeaf275b6731
Hakchin Kim <sn2ro@eerfmai.local>
2016-09-20 02:06:07 +0900

    Initial Commit

---
+++ MyScript/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyScript
+//
+//  Created by Hakchin Kim on 9/20/16.
+//  Copyright © 2016 Hakchin Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
